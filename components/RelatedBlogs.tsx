5846a053b8155c9b5e2a9534c9028e19d76f47c2
“aster-sandeep” <sandeep.shrestha@asterdio.com>
2022-12-27 14:12:26 +0545

    Related Blogs and Featured Property Components Build

---
+++ components/RelatedBlogs.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const RelatedBlogs = () => {
+  return (
+    <>
+      <div className="relatedBlogs">
+        <div className="imageContainer">
+          <img src="" />
+        </div>
+        <div className="contentContainer">
+          <div className="categoryTitle">
+            <p>Title</p>
+          </div>
+          <div className="blogHeader">
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RelatedBlogs;
