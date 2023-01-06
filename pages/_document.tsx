e9c68c1431f1a2c51fbfab8a34f9dfbbca97dd0b
ashim-asterdio <ashrestha@asterdio.com>
2023-01-06 11:06:07 +0545

    main navbar added and design change for category page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/* <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"></link> */}
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"></link>
                     <link rel='icon' type='image/png' href="/Favicon.png" />
                 </Head>
                 <body>
