3a2adb349b48ffe72cd954f423f720660ccd25a0
ashim-asterdio <ashrestha@asterdio.com>
2023-01-06 14:16:22 +0545

    main navbar added

--- layouts/Navbar.tsx
+++ layouts/Navbar.tsx
@@ -6,20 +6,26 @@ import navPerson from "../public/Images/navPerson.svg"
 import type { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router"
+import hamburger from "../public/Images/hamburger.svg"
+import plus from "../public/Images/plus.svg"
+
 // 
 
 
 const Navbar: NextPage = () => {
   const links: any = ["Buy", "Rent", "Home Loans", "Blogs", "Agencies", "Unit Converter"];
-  const router=useRouter()
+  const router = useRouter()
   // const dispatch=useDispatch()
-  
+
 
   return (
     <>
       <div className="nav">
 
         <div className="leftDiv">
+          <div className="hamburger">
+            <Image src={hamburger} alt="no image" />
+          </div>
           <div className="logo">
             <Image src={BasobasLogo} alt="no image" />
           </div>
@@ -28,7 +34,7 @@ const Navbar: NextPage = () => {
               links.map((data: any) =>
                 <Link key={data} href="#">
                   {/* <a href="#" className="links"> */}
-                    <span className="linksData">{data}</span>
+                  <span className="linksData">{data}</span>
                   {/* </a> */}
                 </Link>
               )
@@ -38,9 +44,10 @@ const Navbar: NextPage = () => {
 
         <div className="rightDiv">
           <div className="searchIcon"><Image src={search} alt="no image" /></div>
+          <div className="plus"><Image src={plus} alt="no image" /></div>
           <div className="buttonDiv">
             <button className="blueButton">
-              <span ><Image src={buldingLogo} alt="no image"  /></span>
+              <span ><Image src={buldingLogo} alt="no image" /></span>
               <p>List Your Property</p>
             </button>
             <div className="userDiv">
