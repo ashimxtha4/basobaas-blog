e73e08a7bdd1b0b6842f3d0f7fc2c9a5fe224b1c
“aster-sandeep” <sandeep.shrestha@asterdio.com>
2023-01-18 15:55:31 +0545

    dynamic state set

--- apis/request.ts
+++ apis/request.ts
@@ -1,9 +1,12 @@
+import { stringify } from "querystring";
 import { api } from "./api";
 
 //Blogs
 let blogs = {
-  getBlogs: () => api.get("collections/blogs/records/"),
-};
+  getBlogs: (query: { page?: string | number, perPage?: string | number, category?:string }) =>
+{ 
+ return api.get("collections/blogs/records/"+`${ query.category?`?filter=(category.name_en='${query.category}')`:'?'+stringify(query)}`)
+}};
 
 //Categories
 let category = {
