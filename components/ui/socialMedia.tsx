cb508e58cba06f8bf8f53f98df4917de1069f91e
ashim-asterdio <ashrestha@asterdio.com>
2023-01-30 13:09:01 +0545

    share link feature

--- components/ui/socialMedia.tsx
+++ components/ui/socialMedia.tsx
@@ -13,25 +13,25 @@ import { useRouter } from "next/router";
 const SocialMedia = ({ methode }: { methode: any }) => {
   const router = useRouter();
   const socialMediaList: any = [
-    <FacebookShareButton url="https://basobaasblog.asterdio.xyz">
+    <FacebookShareButton url={window.location.href}>
       <Icon icon="bi:facebook" color="blue" width="40" height="40" />
     </FacebookShareButton>,
-    <EmailShareButton url="https://basobaasblog.asterdio.xyz">
+    <EmailShareButton url={window.location.href}>
       <Icon icon="logos:google-gmail" width="40" height="40" />
     </EmailShareButton>,
-    <LinkedinShareButton url="https://basobaasblog.asterdio.xyz">
+    <LinkedinShareButton url={window.location.href}>
       <Icon icon="mdi:linkedin" color="blue" width="40" height="40" />
     </LinkedinShareButton>,
-    <TelegramShareButton url="https://basobaasblog.asterdio.xyz">
+    <TelegramShareButton url={window.location.href}>
       <Icon icon="logos:telegram" width="40" height="40" />
     </TelegramShareButton>,
-    <TwitterShareButton url="https://basobaasblog.asterdio.xyz">
+    <TwitterShareButton url={window.location.href}>
       <Icon icon="mdi:twitter" color="#1da1f2" width="40" height="40" />
     </TwitterShareButton>,
-    <ViberShareButton url="https://basobaasblog.asterdio.xyz">
+    <ViberShareButton url={window.location.href}>
       <Icon icon="simple-icons:viber" color="purple" width="40" height="40" />
     </ViberShareButton>,
-    <WhatsappShareButton url="https://basobaasblog.asterdio.xyz">
+    <WhatsappShareButton url={window.location.href}>
       <Icon icon="logos:whatsapp-icon" width="40" height="40" />
     </WhatsappShareButton>,
   ];
